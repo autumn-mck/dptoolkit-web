4c996dd09c2248a65d455009f9b87c46f35f980b
Autumn McKee <git@mck.is>
2025-08-08 20:49:07 +0200

    initial loading of data

---
+++ src/datapack.ts
@@ -0,0 +1,85 @@
+import JSZip from "jszip";
+
+export interface Datapack {
+	id: string;
+	name: string;
+	description:
+		| string
+		| {
+				text: string;
+				color: string;
+		  }[];
+	icon: Blob | undefined;
+	mcmeta: Record<string, unknown>;
+	zip: JSZip;
+	config: unknown | undefined;
+	modules: Set<Module>;
+}
+
+const Modules = {
+	STRUCTURE_SET: "structure_set",
+	BIOME: "biome",
+	OVERWORLD: "overworld",
+	DPCONFIG: "dpconfig",
+} as const;
+type Module = (typeof Modules)[keyof typeof Modules];
+
+export async function loadDatapack(file: File): Promise<Datapack | string> {
+	const jsZip = new JSZip();
+	const zip = await jsZip.loadAsync(file);
+
+	const mcmetaText = await zip.file("pack.mcmeta")?.async("string");
+	if (!mcmetaText) {
+		return "Could not load pack.mcmeta. Archive is not a datapack!!! >:(";
+	}
+
+	let mcmeta;
+	try {
+		mcmeta = JSON.parse(mcmetaText);
+	} catch (error) {
+		return "Failed to parse pack.mcmeta";
+	}
+
+	const icon = await zip.file("pack.png")?.async("blob");
+
+	const modules = detectModules(zip);
+
+	let config;
+	if (modules.has(Modules.DPCONFIG)) config = await loadDpConfig(zip);
+
+	return {
+		id: mcmeta.pack.id,
+		name: mcmeta.pack.name,
+		description: mcmeta.pack.description,
+		icon,
+		mcmeta,
+		config,
+		zip,
+		modules,
+	};
+}
+
+async function loadDpConfig(datapackZip: JSZip): Promise<unknown> {
+	const dpConfigText = await datapackZip.file("dpconfig.json")?.async("string");
+	if (dpConfigText) return JSON.parse(dpConfigText);
+	else return {};
+}
+
+function detectModules(datapackZip: JSZip): Set<Module> {
+	const modules = new Set<Module>();
+
+	const matchers: Record<string, Module> = {
+		"/structure_set/": Modules.STRUCTURE_SET,
+		"/worldgen/biome/": Modules.BIOME,
+		"minecraft/dimension/overworld.json": Modules.OVERWORLD,
+		"dpconfig.json": Modules.DPCONFIG,
+	};
+
+	datapackZip.forEach((relativePath, _) => {
+		Object.entries(matchers).forEach(([pattern, mod]) => {
+			if (relativePath.includes(pattern)) modules.add(mod);
+		});
+	});
+
+	return modules;
+}
---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { type Datapack, loadDatapack } from "./datapack";
+
+const fileUploadElement = document.getElementById("input")!;
+fileUploadElement.addEventListener("change", onFileUploaded, { passive: true });
+
+async function onFileUploaded(e: Event) {
+	const fileList = (e.target as HTMLInputElement).files;
+	if (!fileList) return;
+
+	const zipFiles = Array.from(fileList).filter((file) => file.type === "application/zip");
+	const datapacks = await Promise.all(zipFiles.map(loadDatapack));
+
+	console.log(datapacks);
+
+	const dpDisplayElement = datapacks
+		.filter((dp) => dp instanceof Object)
+		.map(createDatapackDisplayElement);
+
+	document.getElementById("datapack-display")!.innerHTML = "";
+	dpDisplayElement.forEach((element) => {
+		document.getElementById("datapack-display")!.appendChild(element);
+	});
+}
+
+function createDatapackDisplayElement(dp: Datapack): DocumentFragment {
+	const template = document.getElementById("datapack-template") as HTMLTemplateElement;
+	const clone = template.content.cloneNode(true) as DocumentFragment;
+
+	(clone.querySelector(".datapack-name") as HTMLElement).innerText = dp.name;
+	(clone.querySelector(".datapack-description") as HTMLElement).innerText =
+		descriptionToDisplayable(dp.description);
+
+	if (dp.icon) {
+		(clone.querySelector(".datapack-icon") as HTMLImageElement).src = URL.createObjectURL(dp.icon);
+	}
+
+	return clone;
+}
+
+function descriptionToDisplayable(description: Datapack["description"]): string {
+	if (Array.isArray(description)) return description.map((desc) => desc.text).join("");
+	else return description;
+}
