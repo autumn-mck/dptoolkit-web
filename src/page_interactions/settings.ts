c0ed8e38d4c838d265a409b8bdb6083c7049065e
Autumn McKee <git@mck.is>
2025-08-17 12:21:49 +0200

    use default archive compression level instead of failing to export if NaN

--- src/page_interactions/settings.ts
+++ src/page_interactions/settings.ts
@@ -2,20 +2,22 @@ import type { CompressionLevel, ExportSettings } from "../types/settings";
 
 export function getExportSettings(): ExportSettings {
 	let element = document.getElementById("setting-archive-compression-level") as HTMLInputElement;
-	const archive_compression_level: number = element.valueAsNumber;
-	if (Number.isNaN(archive_compression_level)) {throw new Error("Archive compression level setting is NaN")};
+	let archive_compression_level = element.valueAsNumber;
+	if (Number.isNaN(archive_compression_level)) archive_compression_level = 0;
 
 	element = document.getElementById("setting-export-modified-only") as HTMLInputElement;
-	const export_modified_only: boolean = element.checked;
+	const export_modified_only = element.checked;
 
 	element = document.getElementById("setting-combine-packs") as HTMLInputElement;
-	const combine: boolean = element.checked;
+	const combine = element.checked;
 
-	console.info(`Getting export settings...\n\tCompression level: ${archive_compression_level}\n\tExport modified only: ${export_modified_only}\n\tCombine: ${combine}`);
+	console.info(
+		`Getting export settings...\n\tCompression level: ${archive_compression_level}\n\tExport modified only: ${export_modified_only}\n\tCombine: ${combine}`
+	);
 
 	return {
 		compressionLevel: archive_compression_level as CompressionLevel,
 		modifiedOnly: export_modified_only,
-		combinePacks: combine
+		combinePacks: combine,
 	};
 }
