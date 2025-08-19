8c8270d9248e632006207d43422c9bc17a99db3e
Bee <117178732+everloste@users.noreply.github.com>
2025-08-19 22:31:57 +0200

    move dialogs, fetch packs from modrinth

--- src/dialogs/datapack_browser.ts
+++ src/dialogs/datapack_browser.ts
@@ -2,7 +2,8 @@ import type { ModrinthProject } from "../types/modrinth_api";
 
 const SupportedPacks: ReadonlyArray<string> = [
 	"geophilic",
-	"explorify"
+	"explorify",
+	"continents"
 ]
 
 async function getSupportedPacks() {
@@ -12,7 +13,26 @@ async function getSupportedPacks() {
 		const response = await fetch(
 			`https://api.modrinth.com/v2/project/${slug}`
 		);
-		const info = await response.json() as ModrinthProject;
+		let info = await response.json() as ModrinthProject;
+
+		const response2 = await fetch(
+			`https://api.modrinth.com/v2/project/${slug}/members`
+		);
+		const info2 = await response2.json() as Array<{user: {username: string; name: string;}}>;
+		info.author = "unknown author";
+		if (info2.length != 0) {
+			const username = info2[0].user.username;
+			info.author = username;
+		}
+		else {
+			if (info.organization) {
+				const response3 = await fetch(
+					`https://api.modrinth.com/v3/organization/${info.organization}`
+				);
+				const info3 = await response3.json() as {name: string;};
+				info.author = info3.name;
+			}
+		}
 		a.push(info);
 	}
 	return a;
@@ -26,15 +46,15 @@ export async function generateDatapackBrowser() {
 	for (const pack of entries) {
 		let temp = document.getElementById("PACK-DOWNLOAD-ITEM-TEMPLATE") as HTMLTemplateElement;
 		console.log(temp);
-		let html_entry = temp.content.firstChild!.cloneNode(true) as HTMLLabelElement;
+		let html_entry = temp.content.querySelector("label")!.cloneNode(true) as HTMLLabelElement;
 		
-		html_entry.id = pack.slug;
-		html_entry.htmlFor = pack.slug;
+		html_entry.id = pack.slug + "-label";
+		html_entry.htmlFor = pack.slug + "-download";
 		(html_entry.querySelector("img") as HTMLImageElement)!.src = pack.icon_url;
 		(html_entry.querySelector(".-name") as HTMLSpanElement)!.innerText = pack.title;
-		(html_entry.querySelector(".-author") as HTMLSpanElement)!.innerText = "unknown";
+		(html_entry.querySelector(".-author") as HTMLSpanElement)!.innerText = pack.author!;
 		(html_entry.querySelector(".-desc") as HTMLSpanElement)!.innerText = pack.description;
-		(html_entry.querySelector("input") as HTMLInputElement)!.id = pack.slug;
+		(html_entry.querySelector("input") as HTMLInputElement)!.id = pack.slug + "-download";
 
 		list_widget.appendChild(html_entry);
 	}
--- src/types/modrinth_api.ts
+++ src/types/modrinth_api.ts
@@ -4,4 +4,6 @@ export type ModrinthProject = {
     description: string;
     icon_url: string;
     categories: Array<string>;
+    author?: string; // this is not returned by modrinth but is added later by the script
+    organization?: string;
 }
