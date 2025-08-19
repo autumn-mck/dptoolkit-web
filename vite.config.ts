e0341df87153c829e74a5257bab3ee6d46182e0a
Autumn McKee <git@mck.is>
2025-08-19 21:49:42 +0200

    use handlepars to split html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import path from "node:path";
 
 export default defineConfig({
 	base: "/dptoolkit-web/",
+	plugins: [
+		handlebars({
+			partialDirectory: path.resolve("./partials"),
+		}),
+	],
 });
